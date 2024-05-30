8089ec745b56135d7707344468a01810fa9cb25d
Max de Bono <debonomax@gmail.com>
2024-05-30 15:37:03 +0100

    fixed get_function_path

--- code/project_helpers/get_project_path.m
+++ code/project_helpers/get_project_path.m
@@ -1,6 +1,6 @@
 function project_path = get_project_path
 FILE_SEPERATOR = "\";
-function_path = matlab.desktop.editor.getActiveFilename;
+function_path = mfilename("fullpath");
 
 path_components = split(function_path,FILE_SEPERATOR);
 project_path = join(path_components(1:(end-3)),FILE_SEPERATOR);
