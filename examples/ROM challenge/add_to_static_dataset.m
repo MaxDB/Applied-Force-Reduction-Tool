9a506dc132cc5075601ba7fa01d7f1ea4aefb33c
gg19546 <gg19546@IT100228>
2024-07-30 11:43:12 +0100

    added rom challenge files to example

---
+++ examples/ROM challenge/add_to_static_dataset.m
@@ -0,0 +1,33 @@
+clear
+close all
+%--------- Software Settings ---------%
+set_logging_level(3)
+set_visualisation_level(0)
+%-------------------------------------%
+
+%--------- System Settings ---------%
+system_name = "exhaust_157";
+added_modes = 6;
+%-----------------------------------%
+
+%--------- Static Solver Settings ---------%
+Static_Opts.num_loadcases = 6;
+Static_Opts.maximum_loadcases = 9;
+%------------------------------------------%
+
+%--------- Static Validation Settings ---------%
+% Validation_Opts.maximum_iterations = 3;
+% Validation_Opts.max_added_points = 50;
+% 
+% Validation_Opts.maximum_iterations = 3;
+% Validation_Opts.max_added_points = 150;
+
+Validation_Opts.maximum_iterations = 2;
+Validation_Opts.max_added_points = 800;
+%------------------------------------------%
+
+load("data\" + system_name + "\" + "Static_Data.mat","Static_Data");
+Static_Data = Static_Data.update_validation_opts(Validation_Opts);
+Static_Data = Static_Data.update_model(added_modes,Static_Opts);
+Static_Data = Static_Data.create_dataset;
+Static_Data.save_data;
