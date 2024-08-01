6e0e90427b69e2f565b8847007fa679862ae9257
MaxDB <debonomax@gmail.com>
2024-08-01 16:12:04 +0100

    Revert "Validation stiffness fitting"

--- code/logging/set_logging_level.m
+++ code/logging/set_logging_level.m
@@ -1,6 +1,6 @@
 function set_logging_level(logging_level)
-if ~isfolder("data")
+if ~exist("data","dir")
     mkdir("data")
 end
-    save("data\log_level.mat","logging_level")
+save("data\log_level.mat","logging_level")
 end
