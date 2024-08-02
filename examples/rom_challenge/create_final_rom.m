ed4e1604fa61b4a80914f9ae56aec19347d529e6
Max de Bono <debonomax@gmail.com>
2024-08-02 10:58:29 +0100

    mixed up files

--- examples/rom_challenge/create_final_rom.m
+++ examples/rom_challenge/create_final_rom.m
@@ -20,7 +20,7 @@ Calibration_Opts.Static_Opts.num_loadcases = 20;
 Static_Opts.static_solver = "abaqus";
 Static_Opts.additional_data = "perturbation";
 Static_Opts.num_validation_modes = 18;
-Static_Opts.max_parallel_jobs = 1; %be careful!
+Static_Opts.max_parallel_jobs = 8; %be careful!
 Static_Opts.num_loadcases = 8;
 Static_Opts.maximum_loadcases = 15;
 %------------------------------------------%
@@ -38,3 +38,43 @@ Model = Dynamic_System(system_name,energy_limit,initial_modes,Calibration_Opts,S
 
 Static_Data = Static_Dataset(Model,Validation_Opts);
 Static_Data.save_data;
+
+%-------------------------------------------------------------------------%
+%-------------------------------------------------------------------------%
+
+Static_Opts.num_loadcases = 6;
+Static_Opts.maximum_loadcases = 9;
+%-------------------------------------------------------------------------%
+added_modes = 7;
+
+
+Validation_Opts.maximum_iterations = 3;
+Validation_Opts.max_added_points = 50;
+
+Static_Data = Static_Data.update_validation_opts(Validation_Opts);
+Static_Data = Static_Data.update_model(added_modes,Static_Opts);
+Static_Data = Static_Data.create_dataset;
+Static_Data.save_data;
+%-------------------------------------------------------------------------%
+
+added_modes = 5;
+
+Validation_Opts.maximum_iterations = 3;
+Validation_Opts.max_added_points = 150;
+
+Static_Data = Static_Data.update_validation_opts(Validation_Opts);
+Static_Data = Static_Data.update_model(added_modes,Static_Opts);
+Static_Data = Static_Data.create_dataset;
+Static_Data.save_data;
+%-------------------------------------------------------------------------%
+
+added_modes = 6;
+
+Validation_Opts.maximum_iterations = 2;
+Validation_Opts.max_added_points = 800;
+
+Static_Data = Static_Data.update_validation_opts(Validation_Opts);
+Static_Data = Static_Data.update_model(added_modes,Static_Opts);
+Static_Data = Static_Data.create_dataset;
+Static_Data.save_data;
+
