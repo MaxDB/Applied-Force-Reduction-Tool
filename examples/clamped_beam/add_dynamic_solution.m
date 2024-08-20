ffab3e97e527555641f19df4dd61dcfe35f39683
Max de Bono <debonomax@gmail.com>
2024-08-20 16:37:53 +0100

    added clamped_beam as an example

---
+++ examples/clamped_beam/add_dynamic_solution.m
@@ -0,0 +1,56 @@
+clear
+close all
+set_visualisation_level(3)
+
+system_name = "clamped_beam_13";
+Dyn_Data = initalise_dynamic_data(system_name);
+
+%-------------------------------------------------------------------------%
+
+%--------- Continuation Settings ---------%
+Continuation_Opts.initial_inc = 1e-2;
+Continuation_Opts.max_inc = 2e-2;
+Continuation_Opts.min_inc = 1e-3;
+Continuation_Opts.forward_steps = 2500;
+Continuation_Opts.backward_steps = 0;
+Continuation_Opts.initial_discretisation_num = 20;
+Continuation_Opts.max_discretisation_num = 250;
+Continuation_Opts.min_discretisation_num = 20;
+Continuation_Opts.collation_degree = 8;
+Continuation_Opts.energy_limit_multiplier = 1;
+%-----------------------------------------%
+
+Dyn_Data = Dyn_Data.add_backbone(1,"opts",Continuation_Opts);
+% compare_validation(Dyn_Data,1,2:18)
+% Dyn_Data = Dyn_Data.validate_solution(1,1:18);
+
+
+
+
+Damping_Data.damping_type = "rayleigh";
+Damping_Data.mass_factor = 10;
+Damping_Data.stiffness_factor = 0;
+
+
+Force_Data.type = "modal";
+Force_Data.mode_number = 1;
+Force_Data.continuation_variable = "frequency";
+Force_Data.amplitude = 2;
+% 
+
+%--------- Continuation Settings ---------%
+Continuation_Opts.initial_inc = 1e-1;
+Continuation_Opts.max_inc = 3e1;
+Continuation_Opts.min_inc = 1e-2;
+Continuation_Opts.forward_steps = 10;
+Continuation_Opts.backward_steps = 500;
+Continuation_Opts.initial_discretisation_num = 20;
+Continuation_Opts.max_discretisation_num = 250;
+Continuation_Opts.min_discretisation_num = 20;
+Continuation_Opts.collation_degree = 6;
+Continuation_Opts.parameter_range = [0.009,0.0209];
+%-----------------------------------------%
+Dyn_Data = Dyn_Data.add_forced_response(Force_Data,Damping_Data,"opts",Continuation_Opts);
+
+
+Dyn_Data = Dyn_Data.get_fe_output("forced_response",2,75);
---
+++ examples/clamped_beam/create_static_dataset.m
@@ -0,0 +1,41 @@
+clear
+close all
+%--------- Software Settings ---------%
+set_logging_level(3)
+set_visualisation_level(0)
+%-------------------------------------%
+
+%--------- System Settings ---------%
+system_name = "clamped_beam";
+energy_limit = 0.02;
+initial_modes = [1,3];
+%-----------------------------------%
+
+%--------- Calibration Settings ---------%
+Calibration_Opts.calibration_scale_factor = 2;
+Calibration_Opts.Static_Opts.num_loadcases = 20;
+%----------------------------------------%
+
+%--------- Static Solver Settings ---------%
+Static_Opts.static_solver = "abaqus";
+Static_Opts.additional_data = "perturbation";
+Static_Opts.num_validation_modes = 18;
+Static_Opts.max_parallel_jobs = 4; %be careful!
+Static_Opts.num_loadcases = 15;
+Static_Opts.maximum_loadcases = 15;
+Static_Opts.minimum_displacement = 5e-8;
+%------------------------------------------%
+
+%--------- Static Validation Settings ---------%
+Validation_Opts.validation_algorithm = "sep_points_new";
+Validation_Opts.minimum_coupling_rating = 1e-2;
+Validation_Opts.maximum_iterations = 3;
+Validation_Opts.maximum_interpolation_error = [1e-3,1e-3];
+Validation_Opts.maximum_fitting_error = 1e-3;
+Validation_Opts.num_added_points = 1;
+%----------------------------------------------%
+
+Model = Dynamic_System(system_name,energy_limit,initial_modes,Calibration_Opts,Static_Opts);
+
+Static_Data = Static_Dataset(Model,Validation_Opts);
+Static_Data.save_data;
