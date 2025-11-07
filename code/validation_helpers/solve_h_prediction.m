cd7b2bb207fc2f8819124b3a5bd1d790b5281176
Max de Bono <debonomax@gmail.com>
2025-11-07 09:17:28 +0000

    renabled validation parallelisation

--- code/validation_helpers/solve_h_prediction.m
+++ code/validation_helpers/solve_h_prediction.m
@@ -39,8 +39,8 @@ time_ranges = zeros(2,num_jobs);
 job_time = zeros(1,num_jobs);
 num_job_orbits = zeros(1,num_jobs);
 
-for iJob = 1:num_jobs
-% parfor (iJob = 1:num_jobs,get_current_parallel_jobs)
+% for iJob = 1:num_jobs
+parfor (iJob = 1:num_jobs,get_current_parallel_jobs)
     time_range = [inf,0];
     num_harmonics = initial_harmonic;
     orbit_group = orbit_groups(iJob,:);
