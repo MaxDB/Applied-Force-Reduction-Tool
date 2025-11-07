71f4ebb563a77f9a90e325f2c2e3c71b4b38bf79
Max de Bono <debonomax@gmail.com>
2025-11-07 16:20:19 +0000

    added helper functions for validation example

---
+++ examples/validation/mems_arch/private/create_mesh.m
@@ -0,0 +1,37 @@
+function num_dof = create_mesh(seed_size)
+SYSTEM_NAME = "mems_arch";
+DIR_DELIMINATOR = "\";
+
+current_directory = pwd;
+script_path =  string(current_directory) + DIR_DELIMINATOR + "generate_input_file" + DIR_DELIMINATOR + "mesh_arch.py";
+geometry_path = string(current_directory) + DIR_DELIMINATOR + "geometry" + DIR_DELIMINATOR + SYSTEM_NAME + DIR_DELIMINATOR;
+
+reset_temp_directory()
+cd generate_input_file
+system("python " + '"' + script_path + '" ' + seed_size);
+copyfile(SYSTEM_NAME + ".inp",geometry_path)
+cd(current_directory)
+
+%--
+files_to_reset = {"matrices.mat","force_calibration.mat","mesh_data.mat"};
+num_files = length(files_to_reset);
+for iFile = 1:num_files
+    file = geometry_path + files_to_reset{iFile};
+    if isfile(file)
+        delete(file)
+    end
+end
+
+%--
+G_ID =fopen(geometry_path + SYSTEM_NAME + ".inp");
+geometry = textscan(G_ID,'%s','delimiter','\n');
+fclose(G_ID);
+geometry = geometry{1,1};
+
+node_position = read_abaqus_node_position(geometry);
+mesh_data = get_mesh_data(geometry);
+mesh_data = mesh_data{1};
+
+num_nodes = size(node_position,1);
+num_dof = num_nodes*mesh_data.dimension;
+end
