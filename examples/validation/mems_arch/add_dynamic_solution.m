76366fc3c4b6c72616287a4015e009432feba14e
Max de Bono <debonomax@gmail.com>
2025-11-24 12:55:32 +0000

    mems_arch example update

--- examples/validation/mems_arch/add_dynamic_solution.m
+++ examples/validation/mems_arch/add_dynamic_solution.m
@@ -4,7 +4,7 @@ close all
 set_visualisation_level(1)
 set_logging_level(3)
 
-system_name = "mems_arch_1";
+system_name = "mems_arch_16";
 Dyn_Data = initalise_dynamic_data(system_name);
 %-------------------------------------------------------------------------%
 Additional_Output.output = "physical displacement";
@@ -19,14 +19,14 @@ Continuation_Opts.max_inc = 5e-1;
 Continuation_Opts.min_inc = 1e-2;
 Continuation_Opts.forward_steps = 200;
 Continuation_Opts.backward_steps = 0;
-Continuation_Opts.collocation_degree = 6;
-Continuation_Opts.initial_discretisation_num = 20;
-Continuation_Opts.min_discretisation_num = 20;
+Continuation_Opts.collocation_degree = 6; %10
+Continuation_Opts.initial_discretisation_num = 40;
 
 % -----------------------------------------%
 
 Dyn_Data = Dyn_Data.add_backbone(1,"opts",Continuation_Opts);
 
+if system_name == "mems_arch_1", return, end
+
 potential_ic = initial_condition_sweep(Dyn_Data.Dynamic_Model,2.69e6,[1.5e-7,1e-7]);
-Continuation_Opts.collocation_degree = 10;
 Dyn_Data = Dyn_Data.add_backbone(1,"ic",potential_ic,"opts",Continuation_Opts);
